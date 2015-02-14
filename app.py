37125f196f28c843c29a59417570d1aa195aa93b
bluegenes <ntpierce@gmail.com>
2015-02-13 17:44:27 -0800

    adding my changes to forked repo

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def index():
 @app.route('/voice', methods=['POST'])
 def voice():
     r = twiml.Response()
-    r.say('Hello Sonya.  Here is a reason why you are awesome.')
+    r.say('Hello Danielle.  Here is a reason why you are awesome.')
     reason = reasonSonyaIsAwesome()
     reason.replace(':', '.')
     reason = "This one is from %s" % reason
@@ -53,7 +53,7 @@ def repeat():
         reason = "This one is from %s" % reason
         r.say(reason)
     elif request.form['Digits'] == '2':
-        r.say('Bye Sonya!')
+        r.say('Bye Danielle!')
         r.hangup()
     else:
         r.say('I did not understand your input.')
@@ -70,8 +70,8 @@ def sms():
     r = twiml.Response()
     reason = reasonSonyaIsAwesome()
     if request.form['Body'].upper() == "HELP":
-        r.sms("Welcome to the Reasons Sonya Is Awesome Hotline.  Text GIMME " \
-                "to get one random reason Sonya is awesome.")
+        r.sms("Welcome to the Reasons Danielle Is Awesome Hotline.  Text GIMME " \
+                "to get one random reason Danielle is awesome.")
     else:
         r.sms(reason)
     return str(r)
@@ -79,38 +79,89 @@ def sms():
 
 def reasonSonyaIsAwesome():
     reasons = [
-            'Alex: You hate the Giants.',
-            'Alex: You can make a mean clafuti.',
-            'Alex: You are an Excel master.',
-            'Alex: You hate Apple.',
-            'Alex: You are a beer snob.',
-            'Alex: You are a great mother.',
-            'Alex: You are a very smart woman.',
-            'Alex: You are the neck of our family.',
-            'Alex: You have a great waist.',
-            'Kent: You are have very intelligent discussions.',
-            'Kent: You have a great sense of humor.',
-            'Ellen: Your brain is is awesome.',
-            'Becca: You are a good listener and has a fantastic perspective.',
-            'Becca: You accomplished everything you hoped to before 30.',
-            'Becca: You were in every Las Vegas casino before she was 18.',
-            'Becca: You can be counted on to be an emergency contact for '\
-                'friends who don\'t have family in NYC.',
-            'Becca: You were at bars in Williamsburg even when you were ' \
-                'super pregnant.',
-            'Becca: You like both opera and ska music.',
-            'Rob: You are one of the most fiercely loyal people in New York.',
-            'Rob: The scope of your intellect is matched only by the size ' \
-                    'of your heart',
-            'Rob: You remember where you came from and are grateful for ' \
-                    'the journey.',
-            'Rob: You and Alex form my ideal of matrimony.',
-            'Rob: Your family comes first.',
-            'Rob: You do not front.',
-            'Bill: You speak Russian.  LOUD.',
-            'Bill: You vote in every election.',
-            'Bill: You are my best friend\'s hit wife.',
-            'Bill: You grew up in Jersey and came out clean.']
+            'Tessa: You are the kindest, most considerate shit I know.',
+            'Tessa: You don\'t always wear glasses, but when you do, they have real lenses.',
+            'Tessa: You are the Shit.',
+            'Tessa: WE ARE THE SHITS.',
+            'Tessa: You are a business MASTER!',
+            'Tessa: Because, SAME BRAIN.',
+            'Tessa: Your ability to consume chicken wings is unparalleled.',
+            'Tessa: o_O',
+            'Tessa: lalalalalalallalalaalalala.',
+            'Tessa: Because AGHHHHHHHHHIAWJFEOIJWAFOIEWA!',
+            'Tessa: Because the NSA must be both concerned and impressed by our level of correspondence.',
+            'Tessa: Because I\'m so lucky we get to walk through life alone together!',
+            'Tessa: \"DUDE THIS GIRL MEANS BUSINESS. CUZ SHE\'S A BUSINESS WOMAN.\"',
+            'Tessa: SLUUUUUUUUURRRRRP',
+            'Tessa: Uhh, Danielle\'s ok, I guess.',
+            'Tessa: Aaaahhhh! Youths!',
+            'Tessa: Because unbridled, hilarious ranting solves everything.',
+            'Tessa: TADDAT. Shuffling together in the right general direction since 1997.',
+            'AA:    It is a little known fact that the refrain \'sweetheart your heart my hot\' in \'I\'m So Hot\' by 183 Club is a reference to Danielle,' \
+                     ' because she is so, so hot.  The part about the \'coh, coh heart\' refers to someone else.',
+            'AA:  The \'abs and glutes\' teacher at Arrillaga still plays Danielle\'s Asian pop CD as pump-up music.',
+            'AA:  Danielle is so talented at multitasking, she can hold an entire conversation with you while she\'s asleep.',
+            'AA:  In her spare time, she teaches sushi noobs how to eat an entire piece of sushi in one bite.' \
+                  ' NO MATTER HOW BIG.  You CAN do it; you WILL do it; and you will LIKE IT.',
+            'AA:  Your ability to consume chicken feet is unparalleled.  ;)',
+            'AA:  Danielle holds the Facebook record for world\'s longest duration of a profile picture.',
+            'AA:   ASES built a shrine in her honor.  As part of their hazing ritual, new members are required to polish it daily ' \
+                    'and spend two hours meditating on how to be more like her.',
+            'AA:  Danielle on a sugar high is THE BEST Danielle.',
+            'AA:  Danielle once broke into a church in the middle of the night, climbed onto its roof through a crazy maze of ceiling-tunnels,' \
+                  'and screamed \"I\'m the king of the world!\" when she got to the top.  Two parts of that story are true.',
+            'AA: Danielle is the best partner-in-crime ever.  She made college so much fun; always game for an adventure. ' \
+                'And she was always there for a hug if I had a bad day.  We solved all of the world\'s problems '\
+                '(because in college, world = boys) during late-night conversations in bed when we both couldn\'t sleep.' \
+                'She is hilarious and goofy and sweet and supportive, and I always felt like I was home no matter where ' \
+                'I was if I was with her!  Love you and miss you, best roomie ever!!! ',
+            'Miko: Dr. Dong\'s vagini coefficient is 0.99, which is 69 standard deviations above the mean.',
+            'Miko: You can pull off talking about economics and stroking a breast and not be Dominic Strauss Kahn.',
+            'Miko: You are the most generous and loyal friend anyone could ever wish for. For reals.  We love you more than you could know.',
+            'Miko: I would go into a knife fight with chopsticks for you.',
+            'Miko: You\'re an honorary blonde when it comes to both beauty and sense of direction (says fellow directionless blonde).',
+            'Miko: The Dr. Dong Cocktail: 2 parts wit, 3 parts heart, 4 parts dead sexy.',
+            'Miko: Cave spelunking eels are nothing compared to your Ravenclaw wand.',
+            'Mui Mui: Thanks for being the best sister ever! You are always so supportive, generous, and caring. ' \
+                   'Thank you for being my role model and best friend. I love you so much! And remember, keep both hands on the steering wheel!',
+            'Jimmy: You are is the world\'s best (inappropriate) haiku writer!',
+            'Jimmy: You boast better baking, basting, and battering skills than Bourdain!',
+            'Jimmy: You paint primroses prettier than Picasso.',
+            'Jimmy: \"10 o\'clock and 2 o\'clock, 10 o\'clock and 2 o\'clock!\" (As an alternate or reinforcing statement for Jacquelyn\'s frequent comment).',
+            'Alan Neo: You\'re a wonderful pengyou to all that know you!',
+            'Jeff: You is the best roommate ever who would never set the microwave on fire.',
+            'Casey: You is the best kind of roommate. And by that, I mean never around, but when she is, she brings food.',
+            'Alexis: Cold hands, warm heart! ',
+            'Alexis: You are a shining example of unconditional love for rabbits!',
+            'Christie: You are incredible. You are vibrant, classic and daring. You are a loyal, adventurous friend, '\
+                       'ready for anything and everything. I love and admire you.',
+            'Kevin: The most beautiful Daniel I ever knew.',
+            'Kevin: Demand-driven. Actionable. Next-generation. Interactive. Ecosystemic. Leveraged. Lateral. EATER.',
+            'Kevin: One woman tiger team.',
+            'Kevin: MARZIPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAN!',
+            'Kevin: The motivation with whom to break fast, the procrastination with whom to lunch, and the inspiration with whom to dream and dine.',
+            'Kevin: Mulier fortis; quasi aurora consurgens.',
+            'Michael: Danielle you are amazing because...        You are a phenomenal dancer             When you speak,  people listen, '\
+                      'You are fantastic at concensus building,             You make it okay to feel vulnerable and uncertain '\
+                      '            You are able to persevere and that inspires us             Your smile radiates feelings of spring time breeze '\
+                      'and summer time warmth.            When you sleep it is funny.',
+            'Brian: Danielle is super caring, kind and loving! Happy Birthday Danielle - You are the best cousin and friend I could ever ask for.',
+            'Judy: Happy Birthday, Daniel(le)!!!',
+            'Judy: Because we put the \"HA\" in White Plaza',
+            'Judy: Two words: ZENG LAOSHI',
+            'Kat: Because she actually liked studying at Meyer.',
+            'Kat: Chinese teachers call her Daniel because they can\'t pronounce Danielle, and she smiles graciously despite it all.',
+            'Kat: She is awesome because she will roll goats with you.',
+            'Kat: Danielle will hug you when she is sick.',
+            'Kat: Danielle\'s wardrobe puts Zooey Deschanel to shame.',
+            'Baba: Danielle is a thoughtful and loving person. She enjoys and values her friendships greatly. '\
+                   'She is a friend to many, definitely a people person. She has many interests and she loves to '\
+                   'explore and learn: the arts, cooking, music, sports, a quick study. Above all, she loves her family!',
+            'Mama: Danielle is a gift from God. She is kind-hearted, thoughtful and always put others before herself. '\
+                   'She is loving, beautiful, gracious, and so much fun to be with. She is also artistic to no end and my '\
+                   'go-to art consultant. I wish our dearest darling Danielle the happiest birthday ever, and may all your '\
+                   'wishes come true! Love you with all my heart!'
+            ]
     return choice(reasons)
 
 if __name__ == '__main__':
--- local_settings.py
+++ local_settings.py
@@ -1,19 +1,21 @@
-'''
-Configuration Settings
+#"""
+#Configuration Settings
+#Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
+#"""
 
-Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
-'''
+# Uncomment to configure in file.
+ACCOUNT_SID = "AC769071c77d687518f99a02229c033113"
+AUTH_TOKEN = "4f3a050634d10862a4d814cce9ae99a6"
+SONYA_APP_SID = "APcd3bb5ab62875814291adf7ddbc5620b"
+#SONYA_CALLER_ID = "+16197291964"
+SONYA_CALLER_ID = "+18589223338"
 
-# Uncommet to configure in file.
-#ACCOUNT_SID = "ACxxxxxxxxxxxxx" 
-#AUTH_TOKEN = "yyyyyyyyyyyyyyyy"
-#SONYA_APP_SID = "APzzzzzzzzz"
-#SONYA_CALLER_ID = "+17778889999"
-
+#for reference, my twilio # = "+16692423359"
 
 # Begin Heroku configuration - configured through environment variables.
 import os
-ACCOUNT_SID = os.environ['ACCOUNT_SID']
-AUTH_TOKEN = os.environ['AUTH_TOKEN']
-SONYA_APP_SID = os.environ['SONYA_APP_SID']
-SONYA_CALLER_ID = os.environ['SONYA_CALLER_ID']
+os.environ['ACCOUNT_SID'] = ACCOUNT_SID
+os.environ['AUTH_TOKEN'] = AUTH_TOKEN
+os.environ['SONYA_APP_SID'] = SONYA_APP_SID
+os.environ['SONYA_CALLER_ID'] = SONYA_CALLER_ID
+
